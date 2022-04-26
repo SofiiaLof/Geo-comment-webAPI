f44c78de5f49677c0ad4ce36e7c1ac7ca2e157e4
Sofiia Löf <sofiia.lof@utb.ecutbildning.se>
2022-04-26 17:36:18 +0200

    Fixade en post-metod där en användare kan lägga till en kommentar

---
+++ GeoComment/Controllers/GeoCommentController.cs
@@ -0,0 +1,49 @@
+﻿using GeoComment.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeoComment.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class GeoCommentController : ControllerBase
+    {
+        private readonly GeoCommentHandler _geoCommentHandler;
+        public GeoCommentController(GeoCommentHandler geoCommentHandler)
+        {
+            _geoCommentHandler = geoCommentHandler;
+        }
+
+        [HttpPost]
+        [Route("geo-comments")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<ActionResult> PostComment(CommentDTO comment)
+        {
+            
+           var postComment = await _geoCommentHandler.PostComment(comment.Author,comment.Message, comment.Longitude, comment.Latitude);
+
+           
+                return Created("",new CommentDTO
+                {
+                    Id = postComment.Id,
+                    Message = postComment.Message,
+                    Author = postComment.User.First_name,
+                    Longitude = postComment.maxLon,
+                    Latitude = postComment.maxLat
+
+                });
+            
+
+        }
+    }
+
+    public class CommentDTO
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public string Author { get; set; }
+        public int Latitude { get; set; }
+        public int Longitude { get; set; }
+    }
+
+}
---
+++ GeoComment/Services/GeoCommentHandler.cs
@@ -0,0 +1,40 @@
+﻿using GeoComment.Data;
+using GeoComment.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeoComment.Services
+{
+    public class GeoCommentHandler
+    {
+
+        private readonly GeoCommentDbContext _ctx;
+
+        public GeoCommentHandler(GeoCommentDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+      
+
+        public async Task<Comment> PostComment(string name,string message, int longitude, int latitude)
+        {
+            var user = await _ctx.Users.Where(u => u.First_name == name).FirstOrDefaultAsync();
+
+            var comment = new Comment
+            {
+                Message = message,
+                User = user,
+                minLat = 0,
+                minLon = 0,
+                maxLat = latitude,
+                maxLon = longitude,
+
+            };
+
+            await _ctx.Comments.AddAsync(comment);
+            await _ctx.SaveChangesAsync();
+
+            return comment;
+        }
+    }
+}
