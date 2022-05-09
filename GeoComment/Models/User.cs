79fa4a4b4eb6c88d5d2632b649867f5a3fe60597
Sofiia Löf <sofiia.lof@utb.ecutbildning.se>
2022-05-09 21:50:18 +0200

    Ändringar som tillhörde identity

--- GeoComment/Models/User.cs
+++ GeoComment/Models/User.cs
@@ -1,10 +1,12 @@
-﻿namespace GeoComment.Models
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace GeoComment.Models
 {
-    public class User
+    public class User : IdentityUser
     {
-        public int Id { get; set; }
+       
         public string First_name { get; set; }
 
-        public IList<Comment> Comments { get; set; }
+        public ICollection<Comment> Comments { get; set; }
     }
 }
