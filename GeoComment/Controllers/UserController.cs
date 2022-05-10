22382a5fdd17acca51239cec856b27b6a3009533
Sofiia Löf <sofiia.lof@utb.ecutbildning.se>
2022-05-10 16:58:59 +0200

    Fixade en post metod för inloggning

--- GeoComment/Controllers/UserController.cs
+++ GeoComment/Controllers/UserController.cs
@@ -1,7 +1,9 @@
 ﻿using System.Runtime.CompilerServices;
+using System.Text.Json.Nodes;
 using System.Text.RegularExpressions;
 using GeoComment.Data;
 using GeoComment.Models;
+using GeoComment.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
@@ -16,10 +18,12 @@ namespace GeoComment.Controllers
     {
         private readonly UserManager<User> _userManager;
         private readonly GeoCommentDbContext _ctx;
-        public UserController(UserManager<User> userManager, GeoCommentDbContext ctx)
+        private readonly JwtPrinter _jwtPrinter;
+        public UserController(UserManager<User> userManager, GeoCommentDbContext ctx, JwtPrinter jwtPrinter)
         {
             _userManager = userManager;
             _ctx = ctx;
+            _jwtPrinter = jwtPrinter;
         }
 
 
@@ -27,7 +31,7 @@ namespace GeoComment.Controllers
         [HttpPost]
         [Route("register")]
         
-        public async Task<ActionResult<RegistrationOutput>> Register(RegistrationInput input)
+        public async Task<ActionResult<UserOutput>> Register(UserInput input)
         {
             var passwordValidator = new PasswordValidator<User>();
             var isValid = await passwordValidator.ValidateAsync(_userManager,null,input.Password);
@@ -48,27 +52,65 @@ namespace GeoComment.Controllers
                 return BadRequest();
             }
 
+          
             await _ctx.SaveChangesAsync();
 
             var registeredUser = await _userManager.Users.Where(u => u.UserName == input.Username).FirstOrDefaultAsync();
-            return Created("", new RegistrationOutput
+          
+            return Created("", new UserOutput
             {
                 Id= registeredUser.Id,
                 Username = registeredUser.UserName
             });
         }
+
+        [ApiVersion("0.2")]
+        [HttpPost]
+        [Route("login")]
+     
+        public async Task<ActionResult<string>> Login(UserInput input)
+        {
+
+
+            var user = await _userManager.FindByNameAsync(input.Username);
+
+            if (user == null)
+            {
+                return BadRequest();
+            }
+
+            var login = await _userManager.CheckPasswordAsync(user, input.Password);
+
+            if (!login)
+            {
+                return BadRequest();
+            }
+
+            var token = _jwtPrinter.Print(user);
+
+
+            return Ok(new TokenOutput
+            {
+                Token = token,
+            });
+        }
     }
 
-    public class RegistrationInput
+    public class UserInput
     {
         public string Username { get; set; }
         public string Password { get; set; }
     }
 
-    public class RegistrationOutput
+    public class UserOutput
     {
         public string Id { get; set; }
         public string Username { get; set; }
        
+    }
+
+    public class TokenOutput
+    {
+        public string Token { get; set; }
     }
 }
